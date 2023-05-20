9b1a98ebded9d3c9ffd48d01e9618a667d755d74
Zulaikha1207 <zulaikhageer@gmail.com>
2023-05-20 10:05:10 +0200

    using HSV and contouring for detection and counting

--- src/counting.py
+++ src/counting.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+import numpy as np
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -9,27 +10,38 @@ args = vars(ap.parse_args())
 
 # Read image and display it
 image = cv2.imread(args["image"])
+image = cv2.resize(image, (750, 500))
+cv2.imshow("resized", image)
+cv2.waitKey(1000)
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# convert to HSV
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+cv2.imshow("hsv image", hsv)
+cv2.waitKey(1000)
 
-# Apply Gaussian blur to reduce noise
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+# Converting it to hue saturation value image
+range1=(26,0,0)
+range2=(86,255,255)
+mask1=cv2.inRange(hsv,range1,range2)
+cv2.imshow("hsv image", mask1)
+cv2.waitKey(1000)
 
-# Perform Canny edge detection
-edges = cv2.Canny(blurred, 50, 150)
+kernel1_size = (7, 7)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel1_size)
+opened_mask = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, kernel)
+closed_mask = cv2.morphologyEx(opened_mask, cv2.MORPH_CLOSE, kernel)
+cv2.imshow("closed image", closed_mask)
+cv2.waitKey(1000)
 
-# Find contours in the edge map
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Draw circles around the detected crops and count them
+contours,h = cv2.findContours(closed_mask, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
 crop_count = 0
 for contour in contours:
     # Calculate the area of the contour
     area = cv2.contourArea(contour)
 
     # Filter contours based on area
-    if area > 1000:
+    if area > 150:
         # Get the center coordinates and radius of the enclosing circle
         (x, y), radius = cv2.minEnclosingCircle(contour)
         center = (int(x), int(y))
